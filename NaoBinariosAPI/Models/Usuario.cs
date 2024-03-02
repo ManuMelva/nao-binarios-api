273b24aef2d76818f4e07f375ee7da1c73591364
Emmanuel Silva <emmanuelsilva101@outlook.com>
2024-03-02 20:22:51 -0300

    Model Validations

--- NaoBinariosAPI/Models/Usuario.cs
+++ NaoBinariosAPI/Models/Usuario.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NaoBinariosAPI.Models
 {
     public class Usuario
     {
         public required int IDUsuario { get; set; }
+
+        [Required(ErrorMessage = "O atributo Nome não pode ser nulo")]
         public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "O atributo Idade não pode ser nulo")]
         public int Idade { get; set; }
+
         public string? Profissao { get; set; }
+
         public int IdadeEmMeses => Idade * 12;
     }
 }
